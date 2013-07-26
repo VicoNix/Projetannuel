5cf8c9015f69f285b9d1a1abf0015eec601c0882
BorisMaric <Belobog@live.fr>
2013-07-26 08:14:17 +0200

    Fin du développement de la fonction retour en base.

---
+++ src/com/project/formviewer/Parser.java
@@ -0,0 +1,375 @@
+package com.project.formviewer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.project.dao.DAOConfigurationException;
+import com.project.dao.DAOFactory;
+
+public class Parser {
+
+	private String xsl;
+	private String xml;
+	private String html;
+	private String requete_select;
+	private ArrayList<String> requetes_insert;
+
+	private ArrayList<String> reponses;
+
+
+	public Parser() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Parser(String xsl, String xml, String html) {
+		super();
+		this.xsl = xsl;
+		this.xml = xml;
+		this.html = html;
+	}
+
+
+	public String getXsl() {
+		return xsl;
+	}
+
+
+	public void setXsl(String xsl) {
+		this.xsl = xsl;
+	}
+
+
+	public String getXml() {
+		return xml;
+	}
+
+
+	public void setXml(String xml) {
+		this.xml = xml;
+	}
+
+
+	public String getHtml() {
+		return html;
+	}
+
+
+	public void setHtml(String html) {
+		this.html = html;
+	}
+
+
+	public ArrayList<String> getReponses() {
+		return reponses;
+	}
+
+
+	public void setReponses(ArrayList<String> reponses) {
+		this.reponses = reponses;
+	}
+	
+	public void parse(){
+		updateXml();
+		formatRequeteInsert();
+		executeRequeteInsert();
+	}
+	
+	
+	private void executeRequeteInsert(){
+		
+		for(String requete : requetes_insert){
+			try {
+				DAOFactory df;
+				ResultSet rs = null;
+				df = DAOFactory.getInstance();
+				Connection con = df.getConnection();
+				Statement state = con.createStatement();
+				rs = state.executeQuery(requete);
+				con.close();
+
+
+			} catch (DAOConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	private void formatRequeteInsert(){
+		requetes_insert = new ArrayList<String>();
+		String champs = requete_select.substring(requete_select.indexOf(" "),requete_select.indexOf("FROM"));
+		String view = requete_select.substring(requete_select.indexOf("FROM")+5,requete_select.indexOf("WHERE"));
+		view = view.replace(" ","");
+		String debut_requete = "INSERT INTO "+view+" ("+champs+") VALUES (";
+		
+		System.out.println("requete insert : "+debut_requete);
+		
+		DocumentBuilderFactory builderFactory_xml = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder_xml = null;
+		
+		
+			try {
+				builder_xml = builderFactory_xml.newDocumentBuilder();
+				Document fichier_xml = builder_xml.parse(new FileInputStream(xml));
+				fichier_xml.getDocumentElement().normalize();
+				
+				Node racine = fichier_xml.getDocumentElement();
+				Element element_racine = (Element) racine;
+				
+				NodeList occurences = fichier_xml.getElementsByTagName(view);
+				System.out.println("Il y a "+occurences.getLength()+" éléments");
+				
+				for(int compteur_occurence=0;compteur_occurence<occurences.getLength();compteur_occurence++){
+					
+					NodeList valeurs = occurences.item(compteur_occurence).getChildNodes();
+					String requete = debut_requete;
+					for(int compteur_de_valeur=0;compteur_de_valeur<valeurs.getLength();compteur_de_valeur++){
+						//Element element = (Element)valeurs.item(compteur_de_valeur);
+						if(valeurs.item(compteur_de_valeur).getNodeType()==Node.ELEMENT_NODE){
+							
+							if(valeurs.item(compteur_de_valeur).toString().contains("DATE")){
+								requete = requete + "to_date('"+valeurs.item(compteur_de_valeur).getTextContent()+"','RRRR-MM-DD'),";
+							}
+							else{
+								requete = requete + "'"+valeurs.item(compteur_de_valeur).getTextContent()+"',";
+							}
+							
+							
+						}
+							
+						//System.out.println(valeurs.item(compteur_de_valeur).getTextContent());
+					}
+					requete = requete.substring(0,requete.lastIndexOf("")-1);
+					requete = requete +")";
+					System.out.println(requete);
+					requetes_insert.add(requete);
+				}
+				
+				
+				
+				
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		
+			
+			
+			
+		
+	}
+
+
+	private void updateXml(){
+
+
+		DocumentBuilderFactory builderFactory_xsl = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder_xsl = null;
+		
+		DocumentBuilderFactory builderFactory_xml = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder_xml = null;
+		
+		try {
+			builder_xsl = builderFactory_xsl.newDocumentBuilder();
+			Document fichier_xsl = builder_xsl.parse(new FileInputStream(xsl));
+			fichier_xsl.getDocumentElement().normalize();
+			
+			builder_xml = builderFactory_xml.newDocumentBuilder();
+			Document fichier_xml = builder_xml.parse(new FileInputStream(xml));
+			fichier_xml.getDocumentElement().normalize();
+			
+			Node racine = fichier_xml.getDocumentElement();
+			Element element_racine = (Element) racine;
+			requete_select = element_racine.getAttribute("query");
+			System.out.println("La requete select vaut : "+requete_select);
+			
+
+			NodeList inputs = fichier_xsl.getDocumentElement().getElementsByTagName("input");
+			int compteur_de_reponse = 0;
+
+			for (int temp = 0; temp < inputs.getLength(); temp++) {
+				Node node = inputs.item(temp);
+
+				Element element = (Element) node;
+				
+				if(element.getAttribute("type").toString().equalsIgnoreCase("submit")){
+
+					
+				}
+				
+				else{
+					
+					boolean isInForEach = false;
+					Node pere = node;
+					while(pere != fichier_xsl.getDocumentElement()){
+						if(pere.getNodeName().contains("for-each")){
+							isInForEach = true;
+						}
+						pere = pere.getParentNode();
+					}
+
+					if(isInForEach==true){
+						if(element.getAttribute("value-of")!=""){
+							
+						}
+						else{
+							NodeList enfants = node.getChildNodes();
+							for(int compteur_de_fils = 0;compteur_de_fils<enfants.getLength();compteur_de_fils++){
+								Node fils =  enfants.item(compteur_de_fils);
+								
+								if (fils.getNodeType() == Node.ELEMENT_NODE  && fils.getNodeName().contains("attribute") ) {
+									NodeList petits_enfants = fils.getChildNodes();
+									for(int compteur_de_petits_fils = 0;compteur_de_petits_fils<petits_enfants.getLength();compteur_de_petits_fils++){
+										Node petit_fils =  petits_enfants.item(compteur_de_petits_fils);
+										
+										if (petit_fils.getNodeType() == Node.ELEMENT_NODE  && petit_fils.getNodeName().contains("value-of") ) {
+											Element element_petit_fils = (Element)petit_fils;
+											
+											String champ_recherche = element_petit_fils.getAttribute("select");
+											champ_recherche = champ_recherche.toUpperCase();
+											
+											
+											NodeList champs = fichier_xml.getDocumentElement().getElementsByTagName(champ_recherche);
+											
+											for (int compteur_de_champs = 0; compteur_de_champs < champs.getLength(); compteur_de_champs++) {
+												
+												Element element_final = (Element)champs.item(compteur_de_champs);
+												
+												champs.item(compteur_de_champs).setTextContent(reponses.get(compteur_de_reponse));
+												compteur_de_reponse++;
+												
+												
+											}
+											
+										}
+									}
+									
+								}
+							}
+						}
+					}
+					else {
+						
+						
+if(element.getAttribute("value-of")!=""){
+							
+						}
+						else{
+							NodeList enfants = node.getChildNodes();
+							for(int compteur_de_fils = 0;compteur_de_fils<enfants.getLength();compteur_de_fils++){
+								Node fils =  enfants.item(compteur_de_fils);
+								
+								if (fils.getNodeType() == Node.ELEMENT_NODE  && fils.getNodeName().contains("attribute") ) {
+									NodeList petits_enfants = fils.getChildNodes();
+									for(int compteur_de_petits_fils = 0;compteur_de_petits_fils<petits_enfants.getLength();compteur_de_petits_fils++){
+										Node petit_fils =  petits_enfants.item(compteur_de_petits_fils);
+										
+										if (petit_fils.getNodeType() == Node.ELEMENT_NODE  && petit_fils.getNodeName().contains("value-of") ) {
+											Element element_petit_fils = (Element)petit_fils;
+											
+											String champ_recherche = element_petit_fils.getAttribute("select");
+											champ_recherche = champ_recherche.toUpperCase();
+											
+											
+											NodeList champs = fichier_xml.getDocumentElement().getElementsByTagName(champ_recherche);
+											
+											for (int compteur_de_champs = 0; compteur_de_champs < champs.getLength(); compteur_de_champs++) {
+												
+												Element element_final = (Element)champs.item(compteur_de_champs);
+												
+												champs.item(compteur_de_champs).setTextContent(reponses.get(compteur_de_reponse));
+												compteur_de_reponse++;
+												
+												
+											}
+											
+										}
+									}
+									
+								}
+							}
+						}
+						
+					}
+					
+				}
+
+			}
+			
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	        Transformer transformer = transformerFactory.newTransformer();
+	        DOMSource source = new DOMSource(fichier_xml);
+	        StreamResult result = new StreamResult(new File(xml));
+	        transformer.transform(source, result);
+			
+
+		} catch (ParserConfigurationException e) {
+			System.out.println("Exception");
+			e.printStackTrace();  
+		}
+		catch (SAXException e) {
+			System.out.println("Exception 2 ");
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			System.out.println("Exception 3");
+			e.printStackTrace();
+
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+
+	}
+
+
+
+
+
+}
--- src/com/project/servlets/FormViewer.java
+++ src/com/project/servlets/FormViewer.java
@@ -1,8 +1,15 @@
 package com.project.servlets;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,104 +24,203 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import com.project.formviewer.Parser;
+
 /**
  * Servlet implementation class FormViewer
  */
 @WebServlet("/FormViewer")
 public class FormViewer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private Parser parser;
+	private boolean repondu = false;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public FormViewer() {
 		super();
-		// TODO Auto-generated constructor stub
+
 	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("on entre dans le doGet");
-		request.setAttribute("filecontentxml", "");
+		repondu = false;
 		request.setAttribute("filecontenthtml", "");
 		this.getServletContext().getRequestDispatcher( "/WEB-INF/FormViewer.jsp" ).forward( request, response );
+
 	}
 
 	/**
+	 * @throws IOException 
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("on entre dans le doPost");
-		String path=request.getParameter("file");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		
-		//Cas ou on a pas encore répondu au formulaire
-		if(path!=null){
-			System.out.println("Path vaut "+path);
-			if(path.substring(path.lastIndexOf('.')).equals(".html"))
+		if(repondu==true){
+			ArrayList<String>reponses = new ArrayList<String>();
+			String parametre = "";
+			int compteur_de_parametre = 0;
+			do{
+				parametre = request.getParameter("element"+compteur_de_parametre);
+				if(parametre !=null)reponses.add(parametre);
+				
+				compteur_de_parametre++;
+			}while(parametre != null);
+			parser.setReponses(reponses);
+			parser.parse();
+			this.getServletContext().getRequestDispatcher( "/WEB-INF/FormulaireRepondu.jsp" ).forward( request, response );
+		}
+		
+		else{
+			repondu = true;
+			parser = new Parser();
+			request.setCharacterEncoding("UTF-8");
+			String xmlajax=request.getParameter("xmlajax");
+			String xslajax=request.getParameter("xslajax");
+			String path=request.getParameter("file");
+			
+			boolean temp=false;
+
+
+			if(path.isEmpty())
 			{
-				Scanner scanner;
-				try {
-					scanner = new Scanner(new FileReader(path.substring(0, path.lastIndexOf('.'))+".xml"));
-					String xml = "";
-					while (scanner.hasNextLine()) {
-						xml += '\n'+scanner.nextLine();
+				path=this.getServletContext().getRealPath("/")+"temp/temp.xml";
+				temp=true;
+			}
+
+			if(path.substring(path.lastIndexOf('.')).equals(".xml")||path.substring(path.lastIndexOf('.')).equals(".xsd")||path.substring(path.lastIndexOf('.')).equals(".xsl")||path.substring(path.lastIndexOf('.')).equals(".html")||temp)
+			{
+
+
+				//HTML Temporaire suite à actualisation
+				if(temp)
+				{
+					StringReader xslreader = new StringReader(xslajax);
+					StringReader xmlreader = new StringReader(xmlajax);
+					StringWriter htmlwriter = new StringWriter();
+					TransformerFactory tFactory = TransformerFactory.newInstance();
+					try {
+						Transformer transformer = tFactory.newTransformer(new StreamSource(xslreader));
+						transformer.transform(new StreamSource(xmlreader), new StreamResult(htmlwriter));
+					} catch (TransformerException e1) {
+
+						e1.printStackTrace();
 					}
-					//System.out.println(xml);
 					
-					request.setAttribute("filecontentxml", xml);
-				} catch (FileNotFoundException e) {
-					request.setAttribute("filecontentxml", "");
+					try {
+						new File(path.substring(0,path.indexOf("temp"))).mkdir(); 
+						BufferedWriter out = new BufferedWriter(new FileWriter(path.substring(0,path.lastIndexOf("."))+".html"));
+						out.write(htmlwriter.toString());
+						out.close();
+					}
+					catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+					request.setAttribute("filecontenthtml", request.getRequestURL().substring(0, request.getRequestURL().lastIndexOf("/"))+path.substring(path.lastIndexOf('/'), path.lastIndexOf('.'))+"/temp.html");
+					request.setAttribute("filecontentxml", xmlajax);
+					request.setAttribute("filecontentxsl", xslajax);
+					System.out.println(xmlajax);
 				}
-				
-				
-				String html_file = request.getRequestURL().substring(0, request.getRequestURL().lastIndexOf("/"))+path.substring(path.lastIndexOf('/'), path.lastIndexOf('.'))+".html";
-				
-				try {
-					scanner = new Scanner(new FileReader(path));
-					String html = "";
-					String ligne_courante = "";
-					int compteur_id = 1;
-					while (scanner.hasNextLine()) {
-						ligne_courante = scanner.nextLine();
-						//System.out.println(ligne_courante);
-						
-						ligne_courante = ligne_courante.replace("<form", "<form method=\"post\" action=\"FormViewer\"");
-						if(ligne_courante.indexOf("value=")>0 && ligne_courante.indexOf("submit")<0){
-							ligne_courante = ligne_courante.replace(" value=", " name=\"element"+compteur_id+"\" value=");
-							compteur_id++;
+				else
+				{
+					//HTML final
+					
+					TransformerFactory tFactory = TransformerFactory.newInstance();
+					try {
+						Transformer transformer = tFactory.newTransformer(new StreamSource(path.substring(0, path.lastIndexOf('.'))+".xsl"));
+						transformer.transform(new StreamSource(path.substring(0, path.lastIndexOf('.'))+".xml"), new StreamResult(path.substring(0, path.lastIndexOf('.'))+".html"));
+					} catch (TransformerException e1) {
+						e1.printStackTrace();
+					}
+					
+					//request.setAttribute("filecontenthtml", request.getRequestURL().substring(0, request.getRequestURL().lastIndexOf("/"))+path.substring(path.lastIndexOf('/'), path.lastIndexOf('.'))+".html");
+					
+							
+					Scanner scanner;
+					
+					try {
+						scanner = new Scanner(new FileReader(path.substring(0, path.lastIndexOf('.'))+".html"));
+						String html = "";
+						String ligne_lue = "";
+						int compteur_de_input = 0;
+						while (scanner.hasNextLine()) {
+							ligne_lue = scanner.nextLine();
+							if(ligne_lue.contains("<input")){
+								ligne_lue = ligne_lue.replace("<input", "<input name=\"element"+compteur_de_input+"\" ");
+								compteur_de_input++;
+							}
+							
+							if(ligne_lue.contains("<form")){
+								ligne_lue = ligne_lue.replace("<form", "<form method=\"POST\" action=\"FormViewer\"");
+							}
+							
+							html += ligne_lue+"\n";
 						}
-						if(ligne_courante.indexOf("</form>")>=0){
-							//html += '\n'+"<input type=\"hidden\" name=\"file\" value=\""+path+"\">";
-						}
-						
-						//System.out.println(ligne_courante);
-						html += '\n'+ligne_courante;
+						parser.setHtml(html);
+						request.setAttribute("filecontenthtml", html);
+					} catch (FileNotFoundException e) {
 						
 					}
-					request.setAttribute("file", path);
-					System.out.println(html);
-					request.setAttribute("filecontenthtml", html);
-				} catch (FileNotFoundException e) {
-					System.out.println("File not found");
-					request.setAttribute("filecontenthtml", "");
+					
+					
+					
+					try {
+						scanner = new Scanner(new FileReader(path.substring(0, path.lastIndexOf('.'))+".xml"));
+						String xml = "";
+						while (scanner.hasNextLine()) {
+							xml += scanner.nextLine()+"\n";
+						}
+						//parser.setXml(xml);
+						parser.setXml(path.substring(0, path.lastIndexOf('.'))+".xml");
+						request.setAttribute("filecontentxml", xml);
+					} catch (FileNotFoundException e) {
+						request.setAttribute("filecontentxml", null);
+					}
+
+					try{
+						scanner = new Scanner(new FileReader(path.substring(0, path.lastIndexOf('.'))+".xsl"));
+						String xsl = "";
+						while (scanner.hasNextLine()) {
+							xsl += scanner.nextLine()+"\n";
+						}
+						parser.setXsl(path.substring(0, path.lastIndexOf('.'))+".xsl");
+						//parser.setXsl(xsl);
+						request.setAttribute("filecontentxsl", xsl);
+					} catch (FileNotFoundException e) {
+						request.setAttribute("filecontentxsl", null);
+					}
+					try{
+						scanner = new Scanner(new FileReader(path.substring(0, path.lastIndexOf('.'))+".xsd"));
+						String xsd = "";
+						while (scanner.hasNextLine()) {
+							xsd += '\n'+scanner.nextLine();
+						}
+						request.setAttribute("filecontentxsd", xsd);
+						
+						scanner.close();
+					}
+					catch (FileNotFoundException e) {
+						request.setAttribute("filecontentxsd", null);
+					}
+					
+					
 				}
-
 				
-				System.out.println();
-				
-				//request.setAttribute("filecontenthtml", html_file);
-
 				
 			}
 			else
 			{
-				request.setAttribute("filecontentxml", "");
-				request.setAttribute("filecontenthtml", "");
+				request.setAttribute("filecontentxml", null);
+				request.setAttribute("filecontentxsl", null);
+				request.setAttribute("filecontentxsd", null);
+				request.setAttribute("filecontenthtml", null);
 			}
 			try {
+				System.out.println(parser.getHtml());
 				this.getServletContext().getRequestDispatcher( "/WEB-INF/FormViewer.jsp" ).forward( request, response );
 			} catch (IOException e) {
 				// TODO Bloc catch g�n�r� automatiquement
@@ -122,20 +228,6 @@ public class FormViewer extends HttpServlet {
 			}
 		}
 		
-		else{
-			ArrayList<String>parametres = new ArrayList<String>();
-			int compteur_parametres = 1;
-			while(true){
-				String parametre = request.getParameter("element"+compteur_parametres);
-				if(parametre==null)break;
-				else parametres.add(parametre);
-				compteur_parametres++;
-			}
-			for(String param : parametres)System.out.println(param);
-			request.setAttribute("filecontenthtml", "");
-			this.getServletContext().getRequestDispatcher( "/WEB-INF/FormViewer.jsp" ).forward( request, response );
-		}
-		
-	}
 
+	}
 }
